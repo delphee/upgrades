1df57458e789bd5bf2f39fbd0fd3301097951da2
delphee <delphee@appletoncampbell.com>
2021-12-17 19:19:07 -0500

    finished

--- scripts/deploy_and_upgrade.py
+++ scripts/deploy_and_upgrade.py
@@ -2,6 +2,7 @@ from scripts.helpful_scripts import (
     encode_function_data,
     get_account,
     encode_function_data,
+    upgrade,
 )
 from brownie import (
     network,
@@ -16,9 +17,9 @@ from brownie import (
 def main():
     account = get_account()
     print(f"Deploying to {network.show_active()}")
-    box = Box.deploy({"from": account})
+    box = Box.deploy({"from": account}, publish_source=True)
 
-    proxy_admin = ProxyAdmin.deploy({"from": account})
+    proxy_admin = ProxyAdmin.deploy({"from": account}, publish_source=True)
 
     # initializer = box.store, 1
     box_encoded_initializer_fuction = (
@@ -30,10 +31,18 @@ def main():
         proxy_admin.address,
         box_encoded_initializer_fuction,
         {"from": account, "gas_limit": 1000000},
+        publish_source=True,
     )
     print(f"Proxy deployed to {proxy}, you can now upgrade to v2")
     proxy_box = Contract.from_abi("Box", proxy.address, Box.abi)
     proxy_box.store(1, {"from": account})
 
-    box_v2 = BoxV2.deploy({"from": account})
-    upgrade()
+    box_v2 = BoxV2.deploy({"from": account}, publish_source=True)
+    upgrade_transaction = upgrade(
+        account, proxy, box_v2.address, proxy_admin_contract=proxy_admin
+    )
+    upgrade_transaction.wait(1)
+    print("proxy has been upgraded!")
+    proxy_box = Contract.from_abi("BoxV2", proxy.address, BoxV2.abi)
+    proxy_box.increment({"from": account})
+    print(proxy_box.retrieve())
